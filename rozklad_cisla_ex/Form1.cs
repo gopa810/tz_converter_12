0a5d5b5d7b9df9e0a5d9b4c093a773d91a88bc4d
gopa810 <root@gopal.home.sk>
2014-07-29 15:40:57 +0200

    rozklad cisla na prvocisla

---
+++ rozklad_cisla_ex/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace rozklad_cisla_ex
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            List<int> arr = new List<int>();
+
+            int cur;
+
+            if (int.TryParse(textBox1.Text, out cur))
+            {
+                bool b = true;
+                while (b)
+                {
+                    b = false;
+                    for (int a = 2; a < cur; a++)
+                    {
+                        if (cur % a == 0)
+                        {
+                            arr.Add(a);
+                            cur = cur / a;
+                            b = true;
+                            break;
+                        }
+                    }
+                }
+                arr.Add(cur);
+                if (arr.Count <= 1)
+                {
+                    label1.Text = cur.ToString() + " je prvocislo";
+                }
+                else
+                {
+                    string str = "";
+                    foreach (int i in arr)
+                    {
+                        if (str.Length > 0)
+                            str += " * ";
+                        str += i.ToString();
+                    }
+                    label1.Text = str;
+                }
+            }
+            else
+            {
+                label1.Text = "Input is not a number.";
+            }
+        }
+    }
+}
---
+++ rozklad_cisla_ex/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace rozklad_cisla_ex
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
